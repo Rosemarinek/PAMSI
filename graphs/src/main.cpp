87c2b9dea84999ae4fadf9c353c1c73a29514985
Aleksandra Rozmus <rosemarinek@gmail.com>
2021-05-10 20:33:08 +0200

    finished main

--- graphs/src/main.cpp
+++ graphs/src/main.cpp
@@ -15,20 +15,34 @@ int main(int argc, char* argv[])
     results.open("../results/result.csv");
     int sourceIndexL;
     int sourceIndexM;
+    int sourceIndexLNeg;
+    int sourceIndexMNeg;
 
     std::vector<std::string> graphFile = {
         "../data/graph/graphV10D0.25.txt",  "../data/graph/graphV10D0.5.txt",   "../data/graph/graphV10D0.75.txt",
-        "../data/graph/graphV10D0.1.txt",   "../data/graph/graphV50D0.25.txt",  "../data/graph/graphV50D0.5.txt",
-        "../data/graph/graphV50D0.75.txt",  "../data/graph/graphV50D0.1.txt",   "../data/graph/graphV100D0.25.txt",
-        "../data/graph/graphV100D0.5.txt",  "../data/graph/graphV100D0.75.txt", "../data/graph/graphV100D0.1.txt",
+        "../data/graph/graphV10D1.txt",     "../data/graph/graphV50D0.25.txt",  "../data/graph/graphV50D0.5.txt",
+        "../data/graph/graphV50D0.75.txt",  "../data/graph/graphV50D1.txt",     "../data/graph/graphV100D0.25.txt",
+        "../data/graph/graphV100D0.5.txt",  "../data/graph/graphV100D0.75.txt", "../data/graph/graphV100D1.txt",
         "../data/graph/graphV150D0.25.txt", "../data/graph/graphV150D0.5.txt",  "../data/graph/graphV150D0.75.txt",
-        "../data/graph/graphV150D0.1.txt",  "../data/graph/graphV200D0.25.txt", "../data/graph/graphV200D0.5.txt",
-        "../data/graph/graphV200D0.75.txt", "../data/graph/graphV200D0.1.txt"};
+        "../data/graph/graphV150D1.txt",    "../data/graph/graphV200D0.25.txt", "../data/graph/graphV200D0.5.txt",
+        "../data/graph/graphV200D0.75.txt", "../data/graph/graphV200D1.txt"};
+
+    std::vector<std::string> graphFileNegative = {
+        "../data/graph/graphV10D0.25Negative.txt",  "../data/graph/graphV10D0.5Negative.txt",
+        "../data/graph/graphV10D0.75Negative.txt",  "../data/graph/graphV10D1Negative.txt",
+        "../data/graph/graphV50D0.25Negative.txt",  "../data/graph/graphV50D0.5Negative.txt",
+        "../data/graph/graphV50D0.75Negative.txt",  "../data/graph/graphV50D1Negative.txt",
+        "../data/graph/graphV100D0.25Negative.txt", "../data/graph/graphV100D0.5Negative.txt",
+        "../data/graph/graphV100D0.75Negative.txt", "../data/graph/graphV100D1Negative.txt",
+        "../data/graph/graphV150D0.25Negative.txt", "../data/graph/graphV150D0.5Negative.txt",
+        "../data/graph/graphV150D0.75Negative.txt", "../data/graph/graphV150D1Negative.txt",
+        "../data/graph/graphV200D0.25Negative.txt", "../data/graph/graphV200D0.5Negative.txt",
+        "../data/graph/graphV200D0.75Negative.txt", "../data/graph/graphV200D1Negative.txt"};
 
     std::vector<std::string> VD = {"10 0.25 ",  "10 0.5 ",   "10 0.75 ",  "10 1 ",     "50 0.25 ",
                                    "50 0.5 ",   "50 0.75 ",  "50 1 ",     "100 0.25 ", "100 0.5 ",
                                    "100 0.75 ", "100 1 ",    "150 0.25 ", "150 0.5 ",  "150 0.75 ",
-                                   "100 1 ",    "200 0.25 ", "200 0.5 ",  "200 0.75 ", "200 0.1 "};
+                                   "100 1 ",    "200 0.25 ", "200 0.5 ",  "200 0.75 ", "200 1 "};
 
     if(results.good())
     {
@@ -37,10 +51,12 @@ int main(int argc, char* argv[])
                 << "Dijkstra-List "
                 << "Dijkstra-Matrix "
                 << "BellmanFord-List "
-                << "BellmanFord-Matrix \n";
+                << "BellmanFord-Matrix "
+                << "BellmanFord-List-Negative "
+                << "BellmanFord-Matrix-Negative \n";
     }
 
-    for(int i = 0; i < 3; ++i)
+    for(int i = 0; i < graphFile.size(); ++i)
     {
         inputFile.open(graphFile[i]);
         auto graphL = AdjacencyListGraph::createGraph(inputFile);
@@ -50,6 +66,16 @@ int main(int argc, char* argv[])
         inputFile.open(graphFile[i]);
         auto graphM = AdjacencyMatrixGraph::createGraph(inputFile);
         inputFile >> sourceIndexM;
+        inputFile.close();
+
+        inputFile.open(graphFileNegative[i]);
+        auto graphLNeg = AdjacencyListGraph::createGraph(inputFile);
+        inputFile >> sourceIndexLNeg;
+        inputFile.close();
+
+        inputFile.open(graphFileNegative[i]);
+        auto graphMNeg = AdjacencyMatrixGraph::createGraph(inputFile);
+        inputFile >> sourceIndexMNeg;
         inputFile.close();
 
         if(results.good())
@@ -74,10 +100,20 @@ int main(int argc, char* argv[])
                 bellmanFord(*graphM, sourceIndexM, result);
                 timerMB.stop();
 
-                results << timerLD.sInterval() << " ";
-                results << timerMD.sInterval() << " ";
-                results << timerLB.sInterval() << " ";
-                results << timerMB.sInterval() << " \n";
+                timerLBNeg.start();
+                bellmanFord(*graphLNeg, sourceIndexL, result);
+                timerLBNeg.stop();
+
+                timerMBNeg.start();
+                bellmanFord(*graphMNeg, sourceIndexM, result);
+                timerMBNeg.stop();
+
+                results << timerLD.msInterval() << " ";
+                results << timerMD.msInterval() << " ";
+                results << timerLB.msInterval() << " ";
+                results << timerMB.msInterval() << " ";
+                results << timerLBNeg.msInterval() << " ";
+                results << timerMBNeg.msInterval() <<" \n";
             }
         }
     }
