c14ab3ca5c6989da009baaa81ad6a811926c4fca
Aleksandra Rozmus <rosemarinek@gmail.com>
2021-05-23 23:08:03 +0200

    initial commit

---
+++ game/include/Game.hpp
@@ -0,0 +1,20 @@
+#ifndef GAME_GAME_HPP
+#define GAME_GAME_HPP
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Window.hpp>
+
+
+class Game {
+private:
+public:
+    Game();
+
+    ~Game();
+
+};
+
+#endif //GAME_GAME_HPP
---
+++ game/src/Game.cpp
@@ -0,0 +1,12 @@
+#include "../include/Game.hpp"
+
+
+Game::Game() {
+
+}
+
+Game::~Game() {
+
+}
+
+
